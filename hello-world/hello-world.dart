14656620086c7d2f937754c393a607da09cdb510
Kenny Vallejo <keenyy1997@gmail.com>
2021-07-07 16:06:23 -0500

    added comments to the hello world file

--- hello-world/hello-world.dart
+++ hello-world/hello-world.dart
@@ -1,4 +1,22 @@
-
+/**
+ * The `main` function will be our unique 'entry point of execution'
+ * into our program 
+ * -  
+ * La funcion `main` sera nuestro unico `punto de entrada en ejecucion`
+ * dentro de nuestro programa
+ */
 void main() {
+  /**
+   * `print` function behaves equals to `console.log` in js 
+   * -  
+   * la funcion `print` se comporta exactamente igual que 
+   * `console.log` en javascript
+   */
   print('Hello World');
+  /**
+   * In dart, we need to finish our sentences with a semicolon (;) 
+   *  -  
+   * En dart, necesitamos finalizar nuestras sentencias con un
+   * punto y coma (;)
+   */
 }
