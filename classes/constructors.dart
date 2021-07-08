c0eecb90f4a8cff4ae3e315f7c503134bc6e63de
Kenny Vallejo <keenyy1997@gmail.com>
2021-07-08 18:59:54 -0500

    first class with just one constructor

---
+++ classes/constructors.dart
@@ -0,0 +1,34 @@
+class MissingParam implements Exception {
+  @override
+  String toString() {
+    return '[MISSING PARAM]';
+  }
+}
+
+class Person {
+
+  String name = '';
+  int age = 0;
+
+  Person ({ String? name, int age = 0 }) {
+    if (name == null)
+      throw MissingParam();
+    if (age <= 0)
+      throw MissingParam();
+
+    this.name = name;
+    this.age = age;
+  }
+
+  @override
+  toString () {
+    return '\t\n[PERSON INFORMATION]\t\nName: ${this.name}\t\nAge: ${this.age}';
+  }
+}
+
+void main(List<String> args) {
+  
+  Person Kenny = Person(age: 23, name: 'Kenny Vallejo');
+
+  print('Kenny => $Kenny');
+}
